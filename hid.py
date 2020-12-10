3ec6fd7da2bdce1d47b530959f3c7bf96e1f126d
afrizalmuchrom <afrizalmuchrom@gmail.com>
2020-12-10 20:29:29 +0700

    pyinstaller execute get hwid

--- hid.py
+++ hid.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import requests
 import subprocess
 import os
@@ -16,6 +17,19 @@ def GetUUID():
 
 md5 = hashlib.md5()
 md5.update((GetUUID()).encode('utf-8'))
-print(md5.hexdigest())
 
-# print(hashlib.sha256(str('xxxx').encode('utf-8')).hexdigest())
+
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 300, height = 300)
+canvas1.pack()
+
+def gen ():  
+    label1 = tk.Label(root, text= md5.hexdigest(), fg='green', font=('helvetica', 12, 'bold'))
+    canvas1.create_window(150, 200, window=label1)
+    
+button1 = tk.Button(text='Get HWID',command=gen, bg='brown',fg='white')
+canvas1.create_window(150, 150, window=button1)
+
+root.mainloop()
